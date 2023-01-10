e3f7aaa8254028e49e88defb01dc4536e7eba115
chajyoti <122098555+chajyoti@users.noreply.github.com>
2023-01-10 18:06:57 +0530

    Add files via upload

---
+++ pythonintro/Jyoti/operator.py
@@ -0,0 +1,36 @@
+a = 75
+b = 50
+# arithmetic operator
+print("a+b= ", a+b)
+print("a-b =", a-b)
+print("a*b =", a*b)
+print("a/b =",a/b)
+
+ # assignment operator
+print("demostration of assignment operator")
+c = 5
+d = 8
+print(c,d)
+c += 5
+d += 2
+print(c,d)
+
+c -= 2
+d -= 4
+print(c,d)
+
+# Comparision operator
+print("demostration of comparision operator")
+e = 9
+f = 6
+print(e==f)
+print(e>f)
+
+# Logical operator
+print("demostration of logical operator")
+
+g = 5
+h = 7
+print(g==h and g>h)
+print(g==h or g<h)
+
